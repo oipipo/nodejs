51de2b7934e6af300de0cca3aa3d145f8b2e49dd
oipipo <rjvasquez1996@gmail.com>
2020-10-26 21:32:27 -0600

    testing with sockets and cpu

--- public/main.js
+++ public/main.js
@@ -1,7 +1,11 @@
 var socket = io.connect('http://localhost:8080', { 'forceNew': true });
-$( document ).ready(function() {
 
+$( document ).ready(function() {
+    
     var counter=0;
+    var table = document.getElementById("myTable");
+
+    
     
     $('#contador').html(counter);
     
@@ -9,10 +13,53 @@ $( document ).ready(function() {
     socket.on('guia', function(data) {
         counter+=1;
         $('#contador').html(counter);
+        socket.emit("actualizar",counter)
     });
+
+    socket.on("metrics",(data)=>
+    {
+        table.innerHTML = "";
+        data=
+        {
+            "memoria_total":data.total,
+            "memoria_libre":data.free,
+            "memoria_usada":data.used,
+        }
+        var datos =[data];
+        
+        var columns = addAllColumnHeaders(datos, table);
+        
+        for (var i = 0; i < datos.length; i++) 
+        {
+            var row$ = $('<tr/>');
+            for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+            var cellValue = datos[i][columns[colIndex]];
+            if (cellValue == null) cellValue = "";
+            row$.append($('<td/>').html(cellValue));
+            }
+            $(table).append(row$);
+        }
+    })
 
     $("#boton").click(()=>{
         counter+=1;
         $('#contador').html(counter);
     })
 });
+function addAllColumnHeaders(myList, table) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+  
+    for (var i = 0; i < myList.length; i++) {
+      var rowHash = myList[i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $(table).append(headerTr$);
+  
+    return columnSet;
+  }
--- server/main.js
+++ server/main.js
@@ -4,15 +4,15 @@ const app = express();
 const http = require("http")
 const server = http.Server(app);
 const key = require("./key")
-
-
+const bodyParser = require("body-parser")
+const si = require('systeminformation');
 
 
 
 const apiurl=""
 
 //config
-app.set('llave', config.llave);
+app.set('llave', key.key);
 //middleware
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,5 +23,28 @@ server.listen(8080, function() {
 });var io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
-    socket.emit('guia','');
+
+    
+        actualizar(socket)
+        socket.on("actualizar",(data)=>
+        {
+            actualizar(socket)
+        })
+        
+    
 });
+
+function actualizar(socket)
+{
+    si.mem()
+    .then(data => {
+        socket.emit("metrics",data);
+        
+    })
+    .catch(error => console.error(error));
+    
+
+    setTimeout(function(){
+        socket.emit('guia','');
+    }, 1000);
+}
